6753830a9c5973f0a3eb88839645e4010b10ce3f
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2017-08-27 23:31:50 +0100

    Create sajweb_queries.sql

---
+++ sajweb_queries.sql
@@ -0,0 +1,72 @@
+# Query: Get total number of registered users
+
+# | Total number of registered users |
+# | XXXX                             |
+
+
+
+# Query: Get total number of registered users who identify as HAWIYRZ
+
+# | Number of HAWIYRZ |
+# | XXXX              |
+
+
+
+# Query: Get total number of users who were born in 1985
+
+# | Number of users |
+# | XXXX            |
+
+
+
+# Query: Get total number of users who identify as Hawiyrz and Gorol
+
+# | Identify | Number of users |
+# | Hawiyrz  | XXXX            |
+# | Gorol    | XXXX            |
+
+
+
+# Query: Get total number of registered users in each saj ordered by number of users
+
+# | SAJ     | Registered users  |
+# | Ostrawa | XXXX              |
+# | Brno    | XXXX              |
+
+
+# Query: Get 10 schools with most users
+
+# | School  | Number of users   |
+# | VUT     | XXX               |
+# | MU      | XXX               |
+
+
+
+# Query: Get 10 users who wrote most articles
+
+# | Name           | SAJ    | Number of articles written |
+# | Jan Prochazka  | Brno   | XXXX                       |
+
+
+
+# Query: Get total number of users registered in each month in 2010
+
+# | Month | Users registered in this month  |
+# | 1     | XX                              |
+# | 2     | XX                              |
+# | 3     | XX                              |
+# | …     |                                 |
+
+
+
+# Query: Get number of student taxi rides done by Look
+
+# | Number of rides done by Look  |
+# | XXXX                          |
+
+
+
+# Query: Find who was riding with Look most often
+
+# | Most common passenger of Look  |
+# | Jan Prochazka                  |
